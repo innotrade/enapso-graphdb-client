f7c68a8b58e216d3125c9c28316dcb5cf920fb9e
Alexander Schulze <aschulze@innotrade.com>
2019-03-06 13:26:08 +0100

    Updated tests to match Enapso conventions.

--- test/test.js
+++ test/test.js
@@ -1,13 +1,13 @@
 // Innotrade Enapso GraphDB Admin Automated Test Suite
 // (C) Copyright 2019 Innotrade GmbH, Herzogenrath, NRW, Germany
 
-const chai = require('chai'); 
-const should = require('chai').should; 
-const expect = require('chai').expect; 
+const chai = require('chai');
+const should = require('chai').should;
+const expect = require('chai').expect;
 const EnapsoGraphDBClient = require("../enapso-graphdb-client");
 const testConfig = require("./config");
 
-const enapsoGraphDBClient = new EnapsoGraphDBClient.Endpoint({
+const graphDBEndpoint = new EnapsoGraphDBClient.Endpoint({
     queryURL: testConfig.queryURL,
     updateURL: testConfig.updateURL,
     username: testConfig.username,
@@ -20,9 +20,8 @@ describe("Query test", () => {
     before(function (done) { setTimeout(function () { done(); }, 500); });
 
     it('It should return a result', (done) => {
-
         let lQuery = "select * where { ?s ?p ?o } limit 1";
-        enapsoGraphDBClient.query(lQuery).then(result=>{
+        graphDBEndpoint.query(lQuery).then(result => {
             expect(result).to.have.a.property("success");
             done();
         })
