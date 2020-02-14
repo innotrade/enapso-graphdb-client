994face62c67900101323ac5af55bd5d1a831d2a
Alexander Schulze <a.schulze@innotrade.de>
2020-02-14 08:25:18 +0100

    Merged changes from master, data type consideration on read, new automated test cases.

--- test/test.js
+++ test/test.js
@@ -3,7 +3,7 @@
 // Author: Alexander Schulze
 
 const chai = require("chai"),
-  chaiHttp = require("chai-http");
+	chaiHttp = require("chai-http");
 const should = require("chai").should;
 const expect = require("chai").expect;
 chai.use(chaiHttp);
@@ -12,31 +12,31 @@ const { EnapsoGraphDBClient } = require("../index");
 const testConfig = require("./config");
 const GRAPHDB_CONTEXT_TEST = "http://ont.enapso.com/test";
 const graphDBEndpoint = new EnapsoGraphDBClient.Endpoint({
-  baseURL: testConfig.baseURL,
-  repository: testConfig.repository,
-  username: testConfig.username,
-  password: testConfig.password,
-  prefixes: testConfig.prefixes
+	baseURL: testConfig.baseURL,
+	repository: testConfig.repository,
+	username: testConfig.username,
+	password: testConfig.password,
+	prefixes: testConfig.prefixes
 });
 
 describe("Query test", () => {
-  before(function(done) {
-    setTimeout(function() {
-      done();
-    }, 500);
-  });
+	before(function (done) {
+		setTimeout(function () {
+			done();
+		}, 500);
+	});
 
-  it("It should return a result", done => {
-    let lQuery = "select * where { ?s ?p ?o } limit 1";
-    graphDBEndpoint.query(lQuery).then(result => {
-      console.log("Success: " + result.success);
-      expect(result).to.have.a.property("success");
-      done();
-    });
-  });
+	it("It should return a result", done => {
+		let lQuery = "select * where { ?s ?p ?o } limit 1";
+		graphDBEndpoint.query(lQuery).then(result => {
+			console.log("Success: " + result.success);
+			expect(result).to.have.a.property("success");
+			done();
+		});
+	});
 
-  it("It expect to  return a status code 400 while performing upation", done => {
-    let lQuery = `
+	it("It expect to  return a status code 400 while performing upation", done => {
+		let lQuery = `
 	with <${GRAPHDB_CONTEXT_TEST}>
 	delete {
 		entest:TestClass rdf:type owl:Class
@@ -47,53 +47,55 @@ describe("Query test", () => {
 	where {
 		entest:TestClass rdf:type owl:Class
 	}`;
-    graphDBEndpoint.update(lQuery).then(result => {
-      console.log("Success: " + result.statusCode);
-      expect(result.statusCode).to.equal(400);
-      done();
-    });
-  });
+		graphDBEndpoint.update(lQuery).then(result => {
+			console.log("Success: " + result.statusCode);
+			expect(result.statusCode).to.equal(400);
+			done();
+		});
+	});
 
-  it("It expect to return only one result", done => {
-    let lQuery = `
+	it("It expect to return only one result", done => {
+		let lQuery = `
 select * where { ?s ?p ?o } limit 1`;
-    graphDBEndpoint.query(lQuery).then(result => {
-      // console.log("Success: " + result.success);
-      console.log("Success: " + JSON.stringify(result));
-      expect(result.results.bindings).to.have.lengthOf(1);
-      done();
-    });
-  });
+		graphDBEndpoint.query(lQuery).then(result => {
+			// console.log("Success: " + result.success);
+			console.log("Success: " + JSON.stringify(result));
+			expect(result.results.bindings).to.have.lengthOf(1);
+			done();
+		});
+	});
 
-  it("Check server is working", done => {
-    // <= Pass in done callback
-    chai
-      .request("http://localhost:7200")
-      .get("/")
-      .end(function() {
-        let lQuery = "select * where { ?s ?p ?o } limit 1";
-        graphDBEndpoint.query(lQuery).then(result => {
-          console.log("Success: " + result.success);
-          expect(result).to.have.a.property("success");
-          done();
-        });
-      });
-  });
-  it("It should return a status code of 500 while insertion", done => {
-    let lQuery = `
+	it("Check server is working", done => {
+		// <= Pass in done callback
+		chai
+			.request("http://localhost:7200")
+			.get("/")
+			.end(function () {
+				let lQuery = "select * where { ?s ?p ?o } limit 1";
+				graphDBEndpoint.query(lQuery).then(result => {
+					console.log("Success: " + result.success);
+					expect(result).to.have.a.property("success");
+					done();
+				});
+			});
+	});
+
+	it("It should return a status code of 500 while insertion", done => {
+		let lQuery = `
         insert data {
             graph <${GRAPHDB_CONTEXT_TEST}> {
                 entest:TestClass rdf:type owl:Class
             }
         }`;
-    graphDBEndpoint.query(lQuery).then(result => {
-      console.log("Success: " + result.statusCode);
-      expect(result.statusCode).to.equal(500);
-      done();
-    });
-  });
-  it("It should return a status code of 400 while deleting ", done => {
-    let lQuery = `
+		graphDBEndpoint.query(lQuery).then(result => {
+			console.log("Success: " + result.statusCode);
+			expect(result.statusCode).to.equal(500);
+			done();
+		});
+	});
+
+	it("It should return a status code of 400 while deleting ", done => {
+		let lQuery = `
         with <${GRAPHDB_CONTEXT_TEST}>
 		delete {
 			entest:TestClassUpdated rdf:type owl:Class
@@ -101,10 +103,11 @@ select * where { ?s ?p ?o } limit 1`;
 		where {
 			entest:TestClassUpdated rdf:type owl:Class
 		}`;
-    graphDBEndpoint.query(lQuery).then(result => {
-      console.log("Success: " + result.statusCode);
-      expect(result.statusCode).to.equal(400);
-      done();
-    });
-  });
+		graphDBEndpoint.query(lQuery).then(result => {
+			console.log("Success: " + result.statusCode);
+			expect(result.statusCode).to.equal(400);
+			done();
+		});
+	});
+	
 });
