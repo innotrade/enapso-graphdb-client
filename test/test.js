b5a5ee39c649cf398c839755ebceaf62f49ee3cb
Alexander Schulze <a.schulze@innotrade.de>
2020-03-20 16:00:18 +0100

    Improving test case story and using defined standards.

--- test/test.js
+++ test/test.js
@@ -3,7 +3,7 @@
 // Author: Alexander Schulze
 
 const chai = require("chai"),
-  chaiHttp = require("chai-http");
+	chaiHttp = require("chai-http");
 const should = require("chai").should;
 const expect = require("chai").expect;
 chai.use(chaiHttp);
@@ -11,14 +11,16 @@ const { EnapsoGraphDBClient } = require("../index");
 
 const testConfig = require("./config");
 const GRAPHDB_CONTEXT_TEST = 'http://ont.enapso.com/test';
-describe("Query test", () => {
-  before(function(done) {
-    setTimeout(function() {
-      done();
-    }, 500);
-  });
 
-  let graphDBEndpoint = new EnapsoGraphDBClient.Endpoint({
+describe("Query test", () => {
+
+	before(function (done) {
+		setTimeout(function () {
+			done();
+		}, 500);
+	});
+
+	let graphDBEndpoint = new EnapsoGraphDBClient.Endpoint({
 		baseURL: testConfig.baseURL,
 		repository: testConfig.repository,
 		prefixes: testConfig.prefixes
@@ -35,66 +37,72 @@ describe("Query test", () => {
 		})
 	});
 
+	// first try to insert a class
+	it("Test to insert a class", (done) => {
+		let lQuery = `
+insert data {
+	graph <${GRAPHDB_CONTEXT_TEST}> {
+		entest:TestClass rdf:type owl:Class
+	}
+}`;
+		graphDBEndpoint.update(lQuery).then(result => {
+			console.log("Success: " + result.success);
+			expect(result).to.have.property('success', true);
+			done();
+		});
+	});
 
-  it("It expect to  return a success while performing upation", done => {
-    let lQuery = `
-    prefix et: <http://ont.enapso.com/test#>
-    with <${GRAPHDB_CONTEXT_TEST}>
-    delete {
-        et:TestClass rdf:type owl:Class
-    }
-    insert {
-        et:TestClassUpdated rdf:type owl:Class
-    }
-    where {
-        et:TestClass rdf:type owl:Class
-    }`;
-   graphDBEndpoint.update(lQuery).then(result => {
-      console.log("Success: " + result.success);
-      expect(result).to.have.property('success', true);
-      done();
-    });
-  });
+	// then try to update that inserted class
+	it("Test to update the inserted class", (done) => {
+		let lQuery = `
+with <${GRAPHDB_CONTEXT_TEST}>
+delete {
+	entest:TestClass rdf:type owl:Class
+}
+insert {
+	entest:TestClassUpdated rdf:type owl:Class
+}
+where {
+	entest:TestClass rdf:type owl:Class
+}`;
+		graphDBEndpoint.update(lQuery).then(result => {
+			console.log("Success: " + result.success);
+			expect(result).to.have.property('success', true);
+			done();
+		});
+	});
 
-  it("It expect to return only one result", done => {
-    let lQuery = `
-select * where { ?s ?p ?o } limit 1`;
-    graphDBEndpoint.query(lQuery).then(result => {
-      // console.log("Success: " + result.success);
-      console.log("Success: " + result.success);
-      expect(result.results.bindings).to.have.lengthOf(1);
-      done();
-    });
-  });
-  it("It should return a success as a true while insertion", done => {
-    let lQuery = `
-    prefix et: <http://ont.enapso.com/test#>
-    insert data {
-        graph <${GRAPHDB_CONTEXT_TEST}> {
-            et:TestClass rdf:type owl:Class
-        }
-    }
-    `;
-    graphDBEndpoint.update(lQuery).then(result => {
-      console.log("Success: " + result.success);
-      expect(result).to.have.property('success', true);
-      done();
-    });
-  });
-  it("It should return a success as a true while deleting ", done => {
-    let lQuery = `
-    prefix et: <http://ont.enapso.com/test#>
-    with <http://ont.enapso.com/test>
-    delete {
-        et:TestClassUpdated rdf:type owl:Class
-    }
-    where {
-        et:TestClassUpdated rdf:type owl:Class
-    }`;
-    graphDBEndpoint.update(lQuery).then(result => {
-      console.log("Success: " + result.success);
-      expect(result).to.have.property('success', true);
-      done();
-    });
-  });
+	// now try to read the updated class
+	it("Test to read the inserted and updated class", (done) => {
+		let lQuery = `
+select ?class 
+where  {
+	graph <${GRAPHDB_CONTEXT_TEST}> {
+		?class a owl:Class
+	}
+} limit 1`;
+		graphDBEndpoint.query(lQuery).then(result => {
+			console.log("Success: " + result.success);
+			expect(result.results.bindings).to.have.lengthOf(1);
+			done();
+		});
+	});
+
+	// and finally try to delete the new and updated class
+	it("Test to the delete the inserted and updated class", (done) => {
+		let lQuery = `
+with <${GRAPHDB_CONTEXT_TEST}>
+delete {
+	entest:TestClassUpdated rdf:type owl:Class
+}
+where {
+	entest:TestClassUpdated rdf:type owl:Class
+}`;
+		graphDBEndpoint.update(lQuery).then(result => {
+			console.log("Success: " + result.success);
+			expect(result).to.have.property('success', true);
+			done();
+		});
+	});
+
 });
