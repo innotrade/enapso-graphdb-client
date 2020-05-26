fab109b2673d421f3f57fcfa8d0e1c2d22d14df3
Ashesh Goplani <ashesh.goplani96@gmail.com>
2020-05-26 08:03:48 +0500

    new changes for error handling

---
+++ demo/demo2.js
@@ -0,0 +1,72 @@
+// Innotrade Enapso GraphDB Client Example
+// (C) Copyright 2019-2020 Innotrade GmbH, Herzogenrath, NRW, Germany
+// Author: Alexander Schulze
+
+const { EnapsoGraphDBClient } = require("../index");
+
+// connection data to the running GraphDB instance
+const GRAPHDB_BASE_URL = "http://localhost:7200",
+	GRAPHDB_REPOSITORY = "Test",
+	GRAPHDB_USERNAME = "Test",
+	GRAPHDB_PASSWORD = "Test",
+	GRAPHDB_CONTEXT_TEST = "http://ont.enapso.com/repo";
+
+const DEFAULT_PREFIXES = [
+	EnapsoGraphDBClient.PREFIX_OWL,
+	EnapsoGraphDBClient.PREFIX_RDF,
+	EnapsoGraphDBClient.PREFIX_RDFS,
+	EnapsoGraphDBClient.PREFIX_XSD,
+	EnapsoGraphDBClient.PREFIX_PROTONS,
+	{
+		prefix: "entest",
+		iri: "http://ont.enapso.com/repo#",
+	},
+];
+
+let graphDBEndpoint = new EnapsoGraphDBClient.Endpoint({
+	baseURL: GRAPHDB_BASE_URL,
+	repository: GRAPHDB_REPOSITORY,
+	prefixes: DEFAULT_PREFIXES,
+	transform: "toCSV",
+});
+
+graphDBEndpoint
+	.login(GRAPHDB_USERNAME, GRAPHDB_PASSWORD)
+	.then((result) => {
+		console.log(result);
+	})
+	.catch((err) => {
+		console.log(err, "here in error");
+	});
+graphDBEndpoint
+	.query(
+		` select *
+    from <${GRAPHDB_CONTEXT_TEST}>
+where {
+    ?class rdf:type owl:Class
+}`
+	)
+	.then((result) => {
+		// console.log(graphDBEndpoint.transformBindingsToResultSet(result));
+		console.log(result);
+	})
+	.catch((err) => {
+		console.log(err, "here in error");
+	});
+
+graphDBEndpoint
+	.update(
+		`
+  insert data {
+    graph <${GRAPHDB_CONTEXT_TEST}> {
+      entest:TestClass rdf:type owl:Class
+    }
+  }`
+	)
+	.then((result) => {
+		// console.log(graphDBEndpoint.transformBindingsToResultSet(result));
+		console.log(result, "hjere in second");
+	})
+	.catch((err) => {
+		console.log(err, "here in error");
+	});
